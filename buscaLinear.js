c5b6e1ab3e2fe29ccdfafbc40b1f4c5dbaca2099
Mário Lanes <mario.lanesjr@gmail.com>
2019-08-07 23:30:02 -0300

    Alteração nas saídas de informação para devolutiva do resultado

--- buscaLinear.js
+++ buscaLinear.js
@@ -1,22 +1,26 @@
 /* Algoritmo de Busca Linear: 
  * O primeiro parâmetro deve ser um Array 
  * O segundo parâmetro é um valor que você deseja pesquisar dentro do seu Array
-*/
+ */
 
 //Estrutura para o algoritmo de Busca Linear
 function conventionalLinearSearch(arr, value){
-    let result = false;
+    if(!Array.isArray(arr)) 
+        return 'O valor inserido no primeiro parâmetro, não é um array';
 
     for(let index = 0; index < arr.length; index++){
         if(arr[index] === value){
-            result = true;
+            return 'O item: '+ value + ' está no índice '+ index +' do Array!';
         }
     }
-    return result;
+    return 'O item: '+ value + ' não foi encontrado no Array' ;
 }
 
 //Estrutura que pensei após analisar o algoritmo de Busca Linear
 function linearSearch(arr, value){
-    if(!Array.isArray(arr)) return false;
-    return arr.filter(index => index == value).length > 0;
+    if(!Array.isArray(arr)) 
+        return 'O valor inserido no primeiro parâmetro, não é um array';
+    
+    let result = arr.findIndex(v => v == value);
+    return result != -1 ? 'O item: '+ value + ' está no índice '+ result +' do Array!' : 'O item: '+ value + ' não foi encontrado no Array';
 }
